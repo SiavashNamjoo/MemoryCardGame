e5e87b3aaef05a6a9f475b96036c6abce94d3a56
Siavash Namjoo <namjoosiavash@outlook.com>
2023-07-23 14:50:36 +0330

    Rename
    index instead of n

--- Memory card game/Assets/_Scripts/Deck.cs
+++ Memory card game/Assets/_Scripts/Deck.cs
@@ -1,13 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class Deck
 {
     public List<Card> card = new List<Card>();
-
+    
     public void EqualCard(int card1, int card2)
     {
         if (card1 == card2)
@@ -20,16 +21,18 @@ public class Deck
         }
     }
 
-    private void GenerateRandomCard()
+    public void GenerateRandomCard()
     {
-        int n = card.Count;
-        while (n > 1)
+        
+        int index = card.Count;
+        
+        while (index > 1)
         {
-            n--;
-            int k = Random.Range(0, n + 1);
+            index--;
+            int k = Random.Range(0, index + 1);
             Card temp = card[k];
-            card[k] = card[n];
-            card[n] = temp;
+            card[k] = card[index];
+            card[index] = temp;
         }
     }
 }
