8561b9727fe9628f0dfc938e270eec09717e953b
Siavash Namjoo <namjoosiavash@outlook.com>
2023-07-23 17:29:10 +0330

    Test for shuffling card

--- Memory card game/Assets/_Scripts/Game.cs
+++ Memory card game/Assets/_Scripts/Game.cs
@@ -2,18 +2,25 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Game : MonoBehaviour
 {
-    private void Update()
+   
+    private void Start()
     {
-        StartGame();
+        Deck deck = new Deck(new List<Card>(){new Card(0), new Card(1),new Card(2),new Card(3),new Card(4),new Card(5),new Card(6),new Card(7)});
+        deck.GenerateRandomCard();
+        foreach (var card in deck.card)
+        {
+            Debug.Log(card.Value);
+        }
     }
 
-    private void StartGame()
+    private static void  StartGame()
     {
-       Card card = new Card(5);
-       Debug.Log(card.Value);
+        //TODO: Make 10 cards, 5 of each
+        
     }
 
     private void EndGame()
