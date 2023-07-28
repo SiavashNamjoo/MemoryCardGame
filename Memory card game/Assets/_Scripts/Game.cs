0459318a9411e206560baa0b02a82609e5ce1894
Siavash Namjoo <namjoosiavash@outlook.com>
2023-07-28 15:04:12 +0330

    Creat a method to call it unity ispector

--- Memory card game/Assets/_Scripts/Game.cs
+++ Memory card game/Assets/_Scripts/Game.cs
@@ -9,6 +9,7 @@ public class Game : MonoBehaviour
 {
     private Deck deck = new Deck(new List<Card>());
     private Card card = new Card(4);
+    private List<Card> cards = new List<Card>();
     private int firstInput = -1;
     private int secondInput = -1;
     private bool allCardsOn = false;
@@ -31,41 +32,12 @@ public class Game : MonoBehaviour
 
     public void OnButtonClick(int index)
     {
-        
+        deck.OnButtonClick(index);
     }
-
+   
+    
     private void Update()
     {
-        // If one of the inputs is -1
-            // return;
-        // Else
-            // Show card[firstInput] and card[SecondInput] : TurnOn
-            // Check card[firstInput] and card[SecondInput]
-                // If they're equal
-                    // Check if all the cards are on
-                        // If they're all on
-                            // Log YOU WIN
-                            // return
-                // Else
-                    // TurnOff
-        if (firstInput == -1 || secondInput == -1)
-        {
-            return;
-        }
-        else
-        {
-            deck.TurnOnCard(firstInput); 
-            deck.TurnOnCard(secondInput);
-            if (!allCardsOn)
-            {
-                Debug.Log("You win!!");
-                return;
-            }
-            else
-            {
-                deck.TurnOffAllCards();
-                return;
-            }
-        }
+      
     }
 }
